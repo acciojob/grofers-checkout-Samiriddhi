a8730ff39750ee34bbfa495ae0a3c02aa3752850
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-09-01 23:35:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-//console.log(getSumBtn);
+ 
 const getSum = () => {
-   let prices = document.querySelectorAll('.prices');
-
-	let totalPrice = 0;
-   for(let i = 0; i < prices.length; i++) {
-       totalPrice += Number(prices[i].textContent);
-   }
-
-let totalRow = document.createElement('tr');
-   let totalCell = document.createElement('td');
-   totalCell.textContent = 'Total: ' + totalPrice;
-   totalRow.appendChild(totalCell);
-
-	let table = document.querySelector('table');
-   table.appendChild(totalRow);
-  
+  getSumBtn.disabled = true;
+  const prices = document.querySelectorAll(".price");
+  let totalPrice = 0;
+  prices.forEach((price) => {
+    const value = parseInt(price.textContent);
+    if (!Number.isNaN(value)) {
+      totalPrice += value;
+    }
+  });
+  const totalPriceRow = document.createElement("tr");
+  totalPriceRow.id = "ans";
+  const totalPriceData = document.createElement("td");
+  const totalPriceAns = document.createElement("td");
+  totalPriceRow.appendChild(totalPriceData);
+  totalPriceRow.appendChild(totalPriceAns);
+  const data = `Total Price (in Rs): `;
+  totalPriceData.append(data);
+  totalPriceAns.append(`${totalPrice}`);
+  const table = document.querySelector("tbody");
+  table.appendChild(totalPriceRow);
 };
-
+ 
 getSumBtn.addEventListener("click", getSum);
-
