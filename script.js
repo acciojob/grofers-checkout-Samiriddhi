7ede9f2718c5becac7fc771c3a9435d484a39e5d
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-09-01 22:09:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
+//console.log(getSumBtn);
 const getSum = () => {
-//Add your code here
+   let prices = document.querySelectorAll('.prices');
+
+	let totalPrice = 0;
+   for(let i = 0; i < prices.length; i++) {
+       totalPrice += Number(prices[i].textContent);
+   }
+
+let totalRow = document.createElement('tr');
+   let totalCell = document.createElement('td');
+   totalCell.textContent = 'Total: ' + totalPrice;
+   totalRow.appendChild(totalCell);
+
+	let table = document.querySelector('table');
+   table.appendChild(totalRow);
   
 };
 
